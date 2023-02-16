49d7fd140f94b9c5143d14255d1b7b1037d31c83
Sebastiaan Valkiers <sebastiaan.valkiers@uantwerpen.be>
2023-02-16 13:20:20 -0800

    Sample from background and perform a hypergeometric test

--- raptcr/constants/datasets.py
+++ raptcr/constants/datasets.py
@@ -10,3 +10,5 @@ def sample_cdr3s(s:int, background:str=FILENAME, aa_col:str='junction_aa') -> li
     n = sum(1 for line in open(FILENAME)) - 1 # number of records in file (excludes header)
     skip = sorted(random.sample(range(1,n+1),n-s)) #the 0-indexed header will not be included in the skip list
     return pd.read_csv(background, sep='\t', skiprows=skip)[aa_col].to_list()
+
+example =  list(pd.read_csv('./raptcr/datasets/example_repertoire.tsv', sep='\t').junction_aa.unique())
